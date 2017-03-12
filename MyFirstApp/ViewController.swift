3828b6d71830810690d08cff809a59de5af02f0b
jackeiwong <iamjackei@gmail.com>
2017-03-12 13:10:22 +0800

    MyFirstApp

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
+    @IBOutlet var quoteLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    
+    
+    @IBAction func whoSaidThisButtonTapper(sender: AnyObject) {
+        if let url = URL(string: "https://www.ted.com/talks/simon_sinek_how_great_leaders_inspire_action") {
+            let safariViewController = SFSafariViewController(url:url)
+            present(safariViewController, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func shareQuote(sender: AnyObject) {
+        let textToShare = quoteLabel.text ?? ""
+        let activityViewController = UIActivityViewController(activityItems: [textToShare], applicationActivities: nil)
+        present(activityViewController, animated: true, completion: nil)
+    }
 }
 
